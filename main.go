dc1aa5ce08f1d67a61e6d51de0ae571a00d14035
GhostbusterJeffrey <GhostbusterJRNC@gmail.com>
2024-11-07 20:37:05 +0100

    CORS fix

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/GhostbusterJeffrey/dap/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
@@ -21,6 +22,11 @@ func main() {
 
 	// Initialize Fiber
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	// Connect to MongoDB
 	config.ConnectDB()
